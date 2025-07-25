5df92c2f5aeec9e9113e3b1dd273fa3fb988a00d
darinz <95009007+darinz@users.noreply.github.com>
2025-07-24 22:45:12 -0700

    Create pca.py

---
+++ problem-sets/v1/ps4/q3/pca.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def pca(X):
+    """
+    Principal Component Analysis (PCA) placeholder.
+    Args:
+        X (np.ndarray): Input data matrix.
+    Returns:
+        U (np.ndarray): Principal components.
+    """
+    # TODO: Implement PCA algorithm
+    raise NotImplementedError('PCA algorithm not implemented.') 
