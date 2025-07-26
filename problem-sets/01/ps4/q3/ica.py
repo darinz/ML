14b7008da96398031354b1ae2f40d56420540c51
darinz <95009007+darinz@users.noreply.github.com>
2025-07-25 17:37:56 -0700

    Create ica.py

---
+++ problem-sets/01/ps4/q3/ica.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def ica(X):
+    """
+    Independent Component Analysis (ICA) placeholder.
+    Args:
+        X (np.ndarray): Input data matrix.
+    Returns:
+        W (np.ndarray): Unmixing matrix.
+    """
+    # TODO: Implement ICA algorithm
+    raise NotImplementedError('ICA algorithm not implemented.') 
